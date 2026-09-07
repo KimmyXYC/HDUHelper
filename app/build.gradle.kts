2ea9e57960bc7f767552a051761c2824dfbcfa29
Kimmy <yucheng.or@isrc.iscas.ac.cn>
2026-09-07 23:46:46 +0800

    chore: release v1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "moe.nepnep.hduhelper"
         minSdk = 33
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
